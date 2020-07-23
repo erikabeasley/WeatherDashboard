63ab67776eb4df3c8772b637674bc4e73e2004b5
Erika <erika@beasleys.com>
2020-07-23 17:12:04 -0400

    Updating html and js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const inputEl = document.getElementById("searchInput");
 const searchEl = document.getElementById("searchBtn");
 
-// const clearEl = document.getElementById("clear-history");
 const nameEl = document.getElementById("currentCity-display");
 const currentImgEl = document.getElementById("currentCity-img");
 
@@ -11,16 +10,16 @@ const currentHumidityEl = document.getElementById("humidity"); 4
 const currentWindEl = document.getElementById("wind-speed");
 const currentUVEl = document.getElementById("UV-index");
 
-// const historyEl = document.getElementById("history");
+const historyEl = document.getElementById("history");
 let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 console.log(searchHistory);
 
 //API Key
-var apiKey = "&appid=afaa8eea1769b4359fd8e07b2efcefbd";
+var apiKey = "3f61d52554cfb25b0d5c75844b1a2a65";
 
 //Function to get current Weather conditions
-function getWeatherConditions(response) {
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+function getWeatherConditions(cityName) {
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" +  apiKey;
 
     $.ajax({
         url: queryURL,
@@ -31,7 +30,7 @@ function getWeatherConditions(response) {
 
             var currentDate = new Date(response.data.dt * 1000);
             var day = currentDate.getDate();
-            var month = currentDate.getMonth() +1;
+            var month = currentDate.getMonth() + 1;
             var year = currentDate.getFullYear();
             //Display (Month/Day/Year)
             nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
@@ -59,13 +58,11 @@ function getWeatherConditions(response) {
                     currentUVEl.append(UVindex);
                 });
         });
-}
 
-//Function to get 5-Day Forecast
-let cityID = response.data.id;
-let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
+    //Display for 5-Day Forecast
+    let cityID = response.data.id;
+    let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
 
-function get5DayForecast(response) {
     $.ajax({
         url: forecastQueryURL,
         method: "GET"
@@ -75,13 +72,13 @@ function get5DayForecast(response) {
 
             var forecastEls = document.querySelectorAll(".forecast");
             //ForLoop- Displays Forecast for each Day in 5Day Forecast
-            for (i = 0, i < forecastEls.length, i++) {
+            for (i = 0; i < forecastEls.length; i++) {
                 forecastEls[i].innerHTML = "";
                 var forecastIndex = i * 8 + 4;
                 var forecastDate = new Date(response.data.list[forecastIndex].dt * 1000);
                 //Get Day, Month, Year
                 var forecastDay = forecastDate.getDate();
-                var forecastMonth = forecastDate.getMonth() +1;
+                var forecastMonth = forecastDate.getMonth() + 1;
                 var forecastYear = forecastDate.getFullYear();
 
                 //Displays (Month/Day/Year) on each forecast
@@ -109,11 +106,43 @@ function get5DayForecast(response) {
 
         })
 
-} 
-
+}
 //Function for Kelvin To Fahrenheit
 function k2f(K) {
     return Math.floor((K - 273.15) * 1.8 + 32);
 }
 
+//Event Listener for Search Button
+searchEl.addEventListener("click", function () {
+    var searchTerm = inputEl.value;
+    getWeatherConditions();
+    searchHistory.push(searchTerm);
+    localStorage.setItem("search", JSON.stringify(searchHistory));
+    renderSearchHistory();
+})
+
+
 //Function for rendering Search History
+function renderSearchHistory() {
+    historyEl.innerHTML = "";
+    for (let i = 0; i < searchHistory.length; i++) {
+        var history = document.createElement("input");
+
+        history.setAttribute("type", "text")
+        // history.setAttribute("readonly","true");
+        history.setAttribute("class", "form-control d-block bg-white");
+        history.setAttribute("value", "searchHistory[i]");
+        //add event listener 
+        history.addEventListener("click", function () {
+            getWeatherConditions(history.value);
+        })
+        //Display search history on page
+        historyEl.append(history);
+    }
+}
+
+//When page loads, this will automaticall display the last city searched for
+renderSearchHistory();
+if (searchHistory.length > 0) {
+    getWeatherConditions(searchHistory[searchHistory.length - 1])
+}
