087d0d01e9ec66e17d7547a82372c1a5fb13f74d
Erika <erika@beasleys.com>
2020-07-23 12:37:43 -0400

    Created function for getting Weather Conditions

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const searchEl = document.getElementById("searchBtn");
 
 // const clearEl = document.getElementById("clear-history");
 const nameEl = document.getElementById("currentCity-display");
-const currentPicEl = document.getElementById("currentCity-img");
+const currentImgEl = document.getElementById("currentCity-img");
 
 const currentTempEl = document.getElementById("temperature");
 const currentHumidityEl = document.getElementById("humidity");4
@@ -27,8 +27,38 @@ function getWeatherConditions(response) {
         method: "GET"
     })
     .then(function (response) {
-
-
-    })
-
+        console.log(response);
+        
+        var currentDate = new Date(response.data.dt*1000);
+        var day = currentDate.getDate();
+        var month = currentDate.getMonth();
+        var year = currentDate.getFullYear();
+        //Display (Month/Day/Year)
+        nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+        //Display weather icon
+        let weatherImg = response.data.weather[0].icon;
+        currentImgEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherImg + "@2x.png");
+        currentImgEl.setAttribute("alt",response.data.weather[0].description);
+        //Display Temp, Humidity, Wind Speed
+        currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + "F";
+        currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+        currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+        //Display UV Index
+        let lat = response.data.coord.lat;
+        let lon = response.data.coord.lon;
+        let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&cnt=1";
+            $.ajax({
+                url: UVQueryURL,
+                method: "GET"
+            })
+            .then(function (response) {
+                let UVindex = document.createElement("span");
+                UVindex.setAttribute("class","badge badge-danger");
+                UVindex.innerHTML = response.data[0].value;
+                currentUVEl.innerHTML = "UV Index: ";
+                currentUVEl.append(UVindex);
+            });
+    });
 }
+
+//Function to get 5-Day Forecast
