2749895836affd58fb8e866a06fc2540c4992a5e
Erika <erika@beasleys.com>
2020-07-22 23:25:22 -0400

    Psuedocode for javascript

--- script.js
+++ script.js
@@ -2,3 +2,33 @@
 var apiKey = "";
 
 //Store Input Value
+
+
+//Get value of the input from user (city)
+
+
+//Clear Input box
+
+
+//Call the api with query url
+
+
+//Ajax for temp, humidity, and wind speed
+
+//Function to get temp and convert to Fahrenheit
+
+
+//Get and Set to Local Storage: Card for weather display, City, City Date, Temp, Humidity, Wind, and Weather Icon
+
+
+//Append content to page
+
+
+
+
+
+//Forecast function Below
+
+
+
+
