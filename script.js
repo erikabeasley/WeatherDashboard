1d64b758f291b9e70d82416b69f6cf95b28553f8
Erika <erika@beasleys.com>
2020-07-24 18:00:44 -0400

    Trying to fix UV Index bug

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ $(document).ready(function () {
         var searchTerm = inputEl.value;
         console.log("searchTerm" + searchTerm);
         getWeatherConditions(city);
-        // searchHistory.push(searchTerm);
-        // localStorage.setItem("search", JSON.stringify(searchHistory));
-        // renderSearchHistory();
+        searchHistory.push(searchTerm);
+        localStorage.setItem("search", JSON.stringify(searchHistory));
+        renderSearchHistory();
     })
 
     var nameEl = $("#currentCity");
@@ -49,34 +49,28 @@ $(document).ready(function () {
                 var day = currentDate.getDate();
                 var month = currentDate.getMonth() + 1;
                 var year = currentDate.getFullYear();
-                console.log(year)
+    
                 //Display (Month/Day/Year)
-                $("nameEl").html(response.name + " (" + month + "/" + day + "/" + year + ") ");
+                nameEl.html(response.name + " (" + month + "/" + day + "/" + year + ") ");
 
-                console.log(nameEl.innerText);
                 //Display weather icon
                 let weatherImg = response.weather[0].icon;
-                console.log("Message1")
                 $("currentImgEl").attr("src", "https://openweathermap.org/img/wn/" + weatherImg + "@2x.png");
-                console.log("Message2")
+                
                 $("currentImgEl").attr("alt", response.weather[0].description);
                 console.log("Message3")
-                //Display Temp, Humidity, Wind Speed
-                $("currentTempEl").html("Temperature: " + k2f(response.main.temp) + " F");
-                console.log(response.main.temp);
-                console.log(currentTempEl.innerHTML);
-                currentHumidityEl.innerHTML = "Humidity: " + response.main.humidity + "%";
-                console.log(currentHumidityEl.innerHTML);
-                currentWindEl.innerHTML = "Wind Speed: " + response.wind.speed + " MPH";
-                console.log(currentWindEl.innerHTML);
 
+                //Display Temp, Humidity, Wind Speed
+                currentTempEl.html("Temperature: " + k2f(response.main.temp) + " F");
+                currentHumidityEl.html("Humidity: " + response.main.humidity + "%");
+                currentWindEl.html("Wind Speed: " + response.wind.speed + " MPH");
                 getUVIndex(response.coord.lat, response.coord.lon, city);
             });
     }
 
     function getUVIndex(lat, lon, city) {
         //Display UV Index  
-        console.log(lat, lon);
+        console.log(lat, lon, city);
         let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=3f61d52554cfb25b0d5c75844b1a2a65" + "&cnt=1";
         $.ajax({
             url: UVQueryURL,
@@ -84,10 +78,18 @@ $(document).ready(function () {
         }).then(function (response) {
             console.log(response);
             let UVindex = document.createElement("span");
-            $("UVindex").attr("class", "badge badge-danger");
-            UVindex.innerHTML = response[0].value;
-            currentUVEl.innerHTML = "UV Index: " + response[0].value;
+            UVindex.setAttribute("class", "badge badge-danger");
+            UVindex.setAttribute("type", "text");
+            UVindex.setAttribute("value", response[0].value);
+            // UVindex.html(response[0].value);
+            
+            console.log(UVindex);
+            var strUVIndex = JSON.stringify(UVindex); 
+            console.log(strUVIndex);
+            currentUVEl.html("UV Index: " + strUVIndex);            
+            
             // currentUVEl.append(UVindex);
+
             console.log(city);
             get5DayForecast(city);
             
@@ -120,25 +122,23 @@ $(document).ready(function () {
                     //Displays (Month/Day/Year) on each forecast
                     var forecastDateEl = document.createElement("p");
                     forecastDateEl.setAttribute("class", "mt-3 mb-0 forecast-date");
-                    forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+                    forecastDateEl.html(forecastMonth + "/" + forecastDay + "/" + forecastYear);
                     forecastEls[i].append(forecastDateEl);
 
                     //Displays Weather Icon under Date (on each forecast)
                     var forecastWeatherEl = document.createElement("img");
                     $("forecastWeatherEl").attr("src", "https://openweathermap.org/img/wn/" + response.list[forecastIndex].weather[0].icon + "@2x.png");
-                    console.log("mess")
                     forecastWeatherEl.setAttribute("alt", response.list[forecastIndex].weather[0].description);
-                    console.log("mess2")
                     forecastEls[i].append(forecastWeatherEl);
 
                     //Displays Temperature under Weather Icon (on each forecast)
                     var forecastTempEl = document.createElement("p");
-                    forecastTempEl.innerHTML = "Temp: " + k2f(response.list[forecastIndex].main.temp) + " &#176F";
+                    forecastTempEl.html("Temp: " + k2f(response.list[forecastIndex].main.temp) + " &#176F");
                     forecastEls[i].append(forecastTempEl);
 
                     //Displays Humidity under Temperature (on each forecast)
                     var forecastHumdityEl = document.createElement("p");
-                    forecastHumdityEl.innerHtml = "Humidity: " + response.list[forecastIndex].main.humidity + "%";
+                    forecastHumdityEl.html("Humidity: " + response.list[forecastIndex].main.humidity + "%");
                     forecastEls[i].append(forecastHumdityEl);
                 }
 
@@ -153,7 +153,7 @@ $(document).ready(function () {
 
     //Function for rendering Search History
     function renderSearchHistory() {
-        historyEl.innerHTML = "";
+        historyEl.html("");
         for (let i = 0; i < searchHistory.length; i++) {
             var history = document.createElement("input");
 
